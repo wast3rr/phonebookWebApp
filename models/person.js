955c026a378ef5a0493f6ec6c53805192500db8d
Karen <95060071+wast3rr@users.noreply.github.com>
2025-01-11 20:51:56 -0500

    Added linting

--- models/person.js
+++ models/person.js
@@ -7,40 +7,40 @@ console.log(url)
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to mongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('connected to mongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true,
-        validate: {
-            validator: function customValidator(value) {
-                // Regex: ^\d{2,3}-\d+$
-                const regex = /^\d{2,3}-\d+$/;
-                return regex.test(value);
-            },
-            message: props => props.value + ' is not a valid format'
-        }
-    },
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function customValidator(value) {
+        // Regex: ^\d{2,3}-\d+$
+        const regex = /^\d{2,3}-\d+$/
+        return regex.test(value)
+      },
+      message: props => props.value + ' is not a valid format'
+    }
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
--- mongo.js
+++ mongo.js
@@ -6,39 +6,39 @@ const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to mongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('connected to mongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 5) {
-    const newPerson = new Person({
-        name: process.argv[3],
-        number: process.argv[4]
-    })
+  const newPerson = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
 
-    newPerson.save().then(result => {
-        console.log('added ', result.name, ' number ', result.number, ' to phonebook')
-        mongoose.connection.close()
-    })
+  newPerson.save().then(result => {
+    console.log('added ', result.name, ' number ', result.number, ' to phonebook')
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('phonebook')
-        result.forEach(person => {
-            console.log(person.name, person.number)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('phonebook')
+    result.forEach(person => {
+      console.log(person.name, person.number)
     })
+    mongoose.connection.close()
+  })
 } else {
-    console.log('Inappropriate amount of arguments')
-    process.exit(1)
+  console.log('Inappropriate amount of arguments')
+  process.exit(1)
 }
